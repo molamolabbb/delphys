365f9a2f871c37e190c4c408dc6574e63c672b7e
slowmoyang <slowmoyang@gmail.com>
2019-04-17 13:19:14 +0900

    Add new selection cut and branches.

--- analysis/interface/TTAllJetsAnalyser.h
+++ analysis/interface/TTAllJetsAnalyser.h
@@ -74,57 +74,87 @@ class TTAllJetsAnalyser : private BaseAnalyser {
   // NOTE Branches
   //////////////////////////////////////////////////////////////////////////////
   // per event
-  Int_t label_;
+  Int_t b_label_;
 
   // unordered set
-  Int_t num_eflow_;
+  Int_t b_num_eflow_;
 
-  std::vector<Float_t> eflow_pt_;
-  std::vector<Float_t> eflow_eta_;
-  std::vector<Float_t> eflow_phi_;
-  std::vector<Int_t>   eflow_charge_;
-  std::vector<Int_t>   eflow_pid_; // PDG id
-  std::vector<Int_t>   eflow_type_; //
+  std::vector<Float_t> b_eflow_pt_;
+  std::vector<Float_t> b_eflow_eta_;
+  std::vector<Float_t> b_eflow_phi_;
+  std::vector<Int_t>   b_eflow_charge_;
+  std::vector<Int_t>   b_eflow_pid_; // PDG id
+  std::vector<Int_t>   b_eflow_type_; // index for Embedding
 
-  Float_t met_;
-  Float_t met_eta_;
-  Float_t met_phi_;
+  Float_t b_met_;
+  Float_t b_met_eta_;
+  Float_t b_met_phi_;
 
-  // 
-  std::vector<Float_t> jet_pt_;
-  std::vector<Float_t> jet_eta_;
-  std::vector<Float_t> jet_phi_;
-  std::vector<Float_t> jet_pid_;
+  // NOTE jet
+  Int_t b_num_b_jets_; // b-jet multiplicity
+  Int_t b_num_b_jets_6_; // b-jet multiplicity in the highest six jets
 
-  std::vector<Int_t> jet_num_chad_;
-  std::vector<Int_t> jet_num_nhad_;
-  std::vector<Int_t> jet_num_electron_;
-  std::vector<Int_t> jet_num_muon_;
-  std::vector<Int_t> jet_num_photon_;
+  std::vector<Float_t> b_jet_pt_;
+  std::vector<Float_t> b_jet_eta_;
+  std::vector<Float_t> b_jet_phi_;
+  std::vector<Float_t> b_jet_mass_;
 
-  std::vector<Float_t> jet_major_axis_;
-  std::vector<Float_t> jet_minor_axis_;
-  std::vector<Float_t> jet_ptd_;
+  std::vector<Int_t> b_jet_num_chad_;
+  std::vector<Int_t> b_jet_num_nhad_;
+  std::vector<Int_t> b_jet_num_electron_;
+  std::vector<Int_t> b_jet_num_muon_;
+  std::vector<Int_t> b_jet_num_photon_;
 
-  std::vector<Bool_t> jet_b_tag_;
-  std::vector<Bool_t> jet_b_dr_matching_;
-  std::vector<Bool_t> jet_b_tracking_;
+  // refer CMS-PAS-JME-13-002
+  std::vector<Float_t> b_jet_major_axis_;
+  std::vector<Float_t> b_jet_minor_axis_;
+  std::vector<Float_t> b_jet_ptd_;
 
-  std::vector<std::vector<Float_t> > con_pt_;
-  std::vector<std::vector<Float_t> > con_deta_;
-  std::vector<std::vector<Float_t> > con_dphi_;
-  std::vector<std::vector<Int_t> > con_charge_;
-  std::vector<std::vector<Int_t> > con_pid_;
-  std::vector<std::vector<Int_t> > con_type_;
+  // refer https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideBTagMCTools#Legacy_parton_based_jet_flavour
+  // std::vector<Bool_t> b_jet_flavor_;
+  // std::vector<Bool_t> b_jet_flavor_algo_;
+  // std::vector<Bool_t> b_jet_flavor_phys_;
+  std::vector<Bool_t> b_jet_b_tag_;
+  std::vector<Bool_t> b_jet_b_tag_algo_;
+  std::vector<Bool_t> b_jet_b_tag_phys_;
+
+  std::vector<Bool_t> b_jet_b_dr_matching_;
+  std::vector<Bool_t> b_jet_b_tracking_;
+
+  // NOTE constituents of jet
+  std::vector<std::vector<Float_t> > b_con_pt_;
+  std::vector<std::vector<Float_t> > b_con_deta_;
+  std::vector<std::vector<Float_t> > b_con_dphi_;
+  std::vector<std::vector<Int_t> >   b_con_charge_;
+  std::vector<std::vector<Int_t> >   b_con_pid_;
+  std::vector<std::vector<Int_t> >   b_con_type_;
 
   //////////////////////////////////////////////////////////////////////////////
   // NOTE selection cut
+  // Refer
+  // https://arxiv.org/abs/1812.10534
+  // Measurement of the top quark mass in the all-jets final state at s√= 13 TeV
+  // and combination with the lepton+jets channel
   //////////////////////////////////////////////////////////////////////////////
-  const Float_t kBMatchingDeltaR_ = 0.3;
-  const Float_t kMinJetPT_ = 45.0f;
+  const Float_t kMinJetPT_ = 30.0f;
   const Float_t kMaxJetEta_ = 2.4f;
-  const Float_t kMinJetMultiplicity_ = 6;
 
+  // Offline Selection
+  // vertex z 24cm, xy 2cm
+  const UInt_t kMinNumJet_ = 6; // PF jets
+  const Int_t kMinNumBJet_ = 2;
+  const Float_t kMinSixthJetPT_ = 40.0f; // GeV
+  // kMinHT_: hadronic activity
+  // It is the scalar p_T sum of all jets in the event
+  const Float_t kMinHT_ = 450.0f; // GeV
+  const Float_t kMaxDeltaRTwoBJets = 2.0;
+
+  // TODO vertex
+  
+
+
+  // my cuts
+  const Float_t kBMatchingDeltaR_ = 0.3;
 };
 
 
--- analysis/src/TTAllJetsAnalyser.cc
+++ analysis/src/TTAllJetsAnalyser.cc
@@ -15,7 +15,7 @@ TTAllJetsAnalyser::TTAllJetsAnalyser(const TString & in_path,
                                      const TString & out_path,
                                      Int_t label)
     : BaseAnalyser(in_path, out_path, "delphys"),
-      label_(label) {
+      b_label_(label) {
   std::cout << "ctor begin" << std::endl;
 
   std::cout << "SetBranchAddress begin" << std::endl;
@@ -43,20 +43,20 @@ void TTAllJetsAnalyser::makeBranch() {
   gInterpreter->GenerateDictionary("vector<vector<Int_t> >", "vector"); 
   gInterpreter->GenerateDictionary("vector<vector<Float_t> >", "vector"); 
 
-  #define BRANCH_(name, suffix) out_tree_->Branch(#name, & name##_ , #name "/" #suffix);
+  #define BRANCH_(name, suffix) out_tree_->Branch(#name, & b_##name##_ , #name "/" #suffix);
   #define BRANCH_I(name) BRANCH_(name, I);
   #define BRANCH_F(name) BRANCH_(name, F);
   #define BRANCH_O(name) BRANCH_(name, O);
 
-  #define BRANCH_A_(name, size, suffix) out_tree_->Branch(#name, & name##_ , #name"["#size"]/"#suffix);
+  #define BRANCH_A_(name, size, suffix) out_tree_->Branch(#name, & b_##name##_ , #name"["#size"]/"#suffix);
   #define BRANCH_AF(name, size)  BRANCH_A_(name, size, F);
 
-  #define BRANCH_V_(name, type) out_tree_->Branch(#name, "vector<"#type">", & name##_ );
+  #define BRANCH_V_(name, type) out_tree_->Branch(#name, "vector<"#type">", & b_##name##_ );
   #define BRANCH_VF(name) BRANCH_V_(name, Float_t);
   #define BRANCH_VI(name) BRANCH_V_(name, Int_t);
   #define BRANCH_VO(name) BRANCH_V_(name, Bool_t);
 
-  #define BRANCH_ANY(name) out_tree_->Branch(#name, & name##_ );
+  #define BRANCH_ANY(name) out_tree_->Branch(#name, & b_##name##_ );
 
   BRANCH_I(label);
 
@@ -75,6 +75,13 @@ void TTAllJetsAnalyser::makeBranch() {
   BRANCH_F(met_phi);
 
   // jet variables
+  BRANCH_I(num_b_jets);
+  BRANCH_I(num_b_jets_6);
+  BRANCH_VF(jet_pt);
+  BRANCH_VF(jet_eta);
+  BRANCH_VF(jet_phi);
+  BRANCH_VF(jet_mass);
+
   BRANCH_VI(jet_num_chad);
   BRANCH_VI(jet_num_nhad);
   BRANCH_VI(jet_num_electron);
@@ -86,6 +93,8 @@ void TTAllJetsAnalyser::makeBranch() {
   BRANCH_VF(jet_ptd);
 
   BRANCH_VO(jet_b_tag);
+  BRANCH_VO(jet_b_tag_algo);
+  BRANCH_VO(jet_b_tag_phys);
   BRANCH_VO(jet_b_dr_matching);
   BRANCH_VO(jet_b_tracking);
 
@@ -105,63 +114,95 @@ void TTAllJetsAnalyser::makeBranch() {
 void TTAllJetsAnalyser::resetBranch() {
   // CAUTION do not reset label
 
-  num_eflow_ = -1;
-  eflow_pt_.clear();
-  eflow_eta_.clear();
-  eflow_phi_.clear();
-  eflow_charge_.clear();
-  eflow_pid_.clear();
-  eflow_type_.clear();
+  b_num_eflow_ = -1;
+  b_eflow_pt_.clear();
+  b_eflow_eta_.clear();
+  b_eflow_phi_.clear();
+  b_eflow_charge_.clear();
+  b_eflow_pid_.clear();
+  b_eflow_type_.clear();
 
-  met_ = -1.0f;
-  met_eta_ = 100.0f;
-  met_phi_ = 100.0f;
+  b_met_ = -1.0f;
+  b_met_eta_ = 100.0f;
+  b_met_phi_ = 100.0f;
 
-  jet_pt_.clear();
-  jet_eta_.clear();
-  jet_phi_.clear();
-  jet_pid_.clear();
+  b_num_b_jets_ = 0;
+  b_num_b_jets_6_ = 0;
 
-  jet_num_chad_.clear();
-  jet_num_nhad_.clear();
-  jet_num_electron_.clear();
-  jet_num_muon_.clear();
-  jet_num_photon_.clear();
+  b_jet_pt_.clear();
+  b_jet_eta_.clear();
+  b_jet_phi_.clear();
+  b_jet_mass_.clear();
 
-  jet_major_axis_.clear();
-  jet_minor_axis_.clear();
-  jet_ptd_.clear();
+  b_jet_num_chad_.clear();
+  b_jet_num_nhad_.clear();
+  b_jet_num_electron_.clear();
+  b_jet_num_muon_.clear();
+  b_jet_num_photon_.clear();
 
-  jet_b_tag_.clear();
-  jet_b_dr_matching_.clear();
-  jet_b_tracking_.clear();
+  b_jet_major_axis_.clear();
+  b_jet_minor_axis_.clear();
+  b_jet_ptd_.clear();
 
-  con_pt_.clear();
-  con_deta_.clear();
-  con_dphi_.clear();
-  con_charge_.clear();
-  con_pid_.clear();
-  con_type_.clear();
+  b_jet_b_tag_.clear();
+  b_jet_b_tag_algo_.clear();
+  b_jet_b_tag_phys_.clear();
 
+  b_jet_b_dr_matching_.clear();
+  b_jet_b_tracking_.clear();
 
+  b_con_pt_.clear();
+  b_con_deta_.clear();
+  b_con_dphi_.clear();
+  b_con_charge_.clear();
+  b_con_pid_.clear();
+  b_con_type_.clear();
 }
 
 
 Bool_t TTAllJetsAnalyser::selectEvent() {
-  if (jets_->GetEntries() < kMinJetMultiplicity_) return false;
-
   // FIXME move clear to reset method. rename resetBranch to reset..?
   selected_jets_.clear();
-  for (Int_t i = 0; i < jets_->GetEntries(); i++) {
-    auto jet = dynamic_cast<const Jet*>(jets_->At(i));
 
-    if(jet->PT < kMinJetPT_) continue;
-    if(std::fabs(jet->Eta) > kMaxJetEta_) continue;
+  UInt_t num_jets = jets_->GetEntries();
+  if (num_jets < kMinNumJet_) return false;
+
+  // the scalar pT sum of all jets in the event
+  Float_t hadronic_activity = 0.0f;
+
+  // Selected events are required to contain at least six jets, at least two
+  // of which have to be tagged as b jets.
+  Int_t num_b_jets = 0;
+  const Jet* bjet0 = nullptr;
+  const Jet* bjet1 = nullptr;
+
+  for (UInt_t i = 0; i < num_jets; i++) {
+    auto jet = dynamic_cast<const Jet*>(jets_->At(i));
+    // std::cout << i << "-th jet PT: " << jet->PT << std::endl;
+
+    hadronic_activity += jet->PT;
+
+    if (jet->PT < kMinJetPT_) continue;
+    if (std::fabs(jet->Eta) > kMaxJetEta_) continue;
+
+    if ((i < 6) and (jet->BTag == 1)) {
+      num_b_jets++;
+
+      if (bjet0 == nullptr) bjet0 = jet;
+      else if (bjet1 == nullptr) bjet1 = jet;
+    }
 
     selected_jets_.push_back(jet);
   }
 
-  if (selected_jets_.size() < kMinJetMultiplicity_) return false;
+  if (hadronic_activity < kMinHT_) return false;
+  if (num_b_jets < kMinNumBJet_) return false;
+
+  Float_t delta_r_bb = bjet0->P4().DeltaR(bjet1->P4());
+  if (delta_r_bb < kMaxDeltaRTwoBJets) return false; 
+
+  if (selected_jets_.size() < kMinNumJet_) return false;
+  if (selected_jets_.at(5)->PT < kMinSixthJetPT_) return false; // 6th
 
   return true;
 }
@@ -180,7 +221,7 @@ void TTAllJetsAnalyser::fillEFlow() {
   Int_t num_nhads = eflow_neutral_hadrons_->GetEntries();
   Int_t num_photons = eflow_photons_->GetEntries();
 
-  num_eflow_ = num_tracks + num_nhads + num_photons;
+  b_num_eflow_ = num_tracks + num_nhads + num_photons;
 
   // for (Int_t idx = 0; idx < eflow_tracks_->GetEntries(); idx++) {
   for (Int_t idx = 0; idx < num_tracks; idx++) {
@@ -189,34 +230,34 @@ void TTAllJetsAnalyser::fillEFlow() {
     Int_t charge = track->Charge;
     Int_t pf_index = getPFIndex(pid, charge);
 
-    eflow_pt_.push_back(track->PT);
-    eflow_eta_.push_back(track->Eta);
-    eflow_phi_.push_back(track->Phi);
-    eflow_charge_.push_back(track->Charge);
-    eflow_pid_.push_back(pid);
-    eflow_type_.push_back(pf_index);
+    b_eflow_pt_.push_back(track->PT);
+    b_eflow_eta_.push_back(track->Eta);
+    b_eflow_phi_.push_back(track->Phi);
+    b_eflow_charge_.push_back(track->Charge);
+    b_eflow_pid_.push_back(pid);
+    b_eflow_type_.push_back(pf_index);
   }
 
   // for (Int_t idx = 0; idx < eflow_neutral_hadrons_->GetEntries(); idx++) {
   for (Int_t idx = 0; idx < num_nhads; idx++) {
     auto nhad = dynamic_cast<Tower*>(eflow_neutral_hadrons_->At(idx));
-    eflow_pt_.push_back(nhad->ET);
-    eflow_eta_.push_back(nhad->Eta);
-    eflow_phi_.push_back(nhad->Phi);
-    eflow_charge_.push_back(0);
-    eflow_pid_.push_back(pdgid::kNeutralHadron);
-    eflow_type_.push_back(pfindex::kNeutralHadron);
+    b_eflow_pt_.push_back(nhad->ET);
+    b_eflow_eta_.push_back(nhad->Eta);
+    b_eflow_phi_.push_back(nhad->Phi);
+    b_eflow_charge_.push_back(0);
+    b_eflow_pid_.push_back(pdgid::kNeutralHadron);
+    b_eflow_type_.push_back(pfindex::kNeutralHadron);
   }
 
   // for (Int_t idx = 0; idx < eflow_photons_->GetEntries(); idx++) {
   for (Int_t idx = 0; idx < num_photons; idx++) {
     auto photon = dynamic_cast<Tower*>(eflow_photons_->At(idx));
-    eflow_pt_.push_back(photon->ET);
-    eflow_eta_.push_back(photon->Eta);
-    eflow_phi_.push_back(photon->Phi);
-    eflow_charge_.push_back(0);
-    eflow_pid_.push_back(pdgid::kPhoton);
-    eflow_type_.push_back(pfindex::kPhoton);
+    b_eflow_pt_.push_back(photon->ET);
+    b_eflow_eta_.push_back(photon->Eta);
+    b_eflow_phi_.push_back(photon->Phi);
+    b_eflow_charge_.push_back(0);
+    b_eflow_pid_.push_back(pdgid::kPhoton);
+    b_eflow_type_.push_back(pfindex::kPhoton);
   }
 
 }
@@ -238,17 +279,16 @@ Bool_t TTAllJetsAnalyser::trackBottomQuark(const GenParticle* p) {
 
 
 Float_t TTAllJetsAnalyser::getBDaughterRatio(const Jet* jet) {
-  Int_t num_particles = jet->Particles.GetEntries();
-
+  Int_t num_daughters = jet->Particles.GetEntries();
   Int_t num_from_b = 0;
-  for (Int_t idx = 0; idx < num_particles; idx++) {
-    auto p = dynamic_cast<const GenParticle*>(jet->Particles[idx]);
-    if (trackBottomQuark(p)) {
+  for (Int_t idx = 0; idx < num_daughters; idx++) {
+    auto dau = dynamic_cast<GenParticle*>(jet->Particles.At(idx));
+    if (trackBottomQuark(dau)) {
       num_from_b++;
     }
   }
 
-  Float_t b_ratio = static_cast<Float_t>(num_from_b) / num_particles;
+  Float_t b_ratio = static_cast<Float_t>(num_from_b) / num_daughters;
   return b_ratio;
 }
 
@@ -266,23 +306,33 @@ void TTAllJetsAnalyser::fillJetVariables() {
   // FIXME make fillJetVaariable to return boolean
   // if (bottom_quarks.size() != 2) return false
 
+  Int_t jet_count = 0;
   for (const auto & jet : selected_jets_) {
+    jet_count++;
+
     TLorentzVector jet_p4 = jet->P4();
     Float_t j_pt = jet->PT;
     Float_t j_eta = jet->Eta;
     Float_t j_phi = jet->Phi;
 
-    jet_pt_.push_back(j_pt);
-    jet_eta_.push_back(j_eta);
-    jet_phi_.push_back(j_phi);
+    if (jet->BTag == 1) {
+      b_num_b_jets_++;
+    
+      if(jet_count <= 6) b_num_b_jets_6_++;
+    }
+
+    b_jet_pt_.push_back(j_pt);
+    b_jet_eta_.push_back(j_eta);
+    b_jet_phi_.push_back(j_phi);
+    b_jet_mass_.push_back(jet->Mass);
 
     // Loop over cons
-    con_pt_.push_back(std::vector<Float_t>());
-    con_deta_.push_back(std::vector<Float_t>());
-    con_dphi_.push_back(std::vector<Float_t>());
-    con_charge_.push_back(std::vector<Int_t>());
-    con_pid_.push_back(std::vector<Int_t>());
-    con_type_.push_back(std::vector<Int_t>());
+    b_con_pt_.push_back(std::vector<Float_t>());
+    b_con_deta_.push_back(std::vector<Float_t>());
+    b_con_dphi_.push_back(std::vector<Float_t>());
+    b_con_charge_.push_back(std::vector<Int_t>());
+    b_con_pid_.push_back(std::vector<Int_t>());
+    b_con_type_.push_back(std::vector<Int_t>());
     
     Int_t num_chad = 0, num_nhad = 0;
     Int_t num_electron = 0, num_muon = 0, num_photon = 0;
@@ -334,60 +384,61 @@ void TTAllJetsAnalyser::fillJetVariables() {
       Float_t c_deta = c_eta - j_eta;
       Float_t c_dphi = TVector2::Phi_mpi_pi(c_phi - j_phi);
 
-      con_pt_.back().push_back(c_pt);
-      con_deta_.back().push_back(c_deta);
-      con_dphi_.back().push_back(c_dphi);
-      con_charge_.back().push_back(c_charge);
-      con_pid_.back().push_back(c_pid);
-      con_type_.back().push_back(c_type);
+      b_con_pt_.back().push_back(c_pt);
+      b_con_deta_.back().push_back(c_deta);
+      b_con_dphi_.back().push_back(c_dphi);
+      b_con_charge_.back().push_back(c_charge);
+      b_con_pid_.back().push_back(c_pid);
+      b_con_type_.back().push_back(c_type);
 
     } // end loop over cons
 
     ////////////////////////////////////////////////////////////////////////////
     // NOTE CMS Variables
     ////////////////////////////////////////////////////////////////////////////
-    jet_num_chad_.push_back(num_chad);
-    jet_num_nhad_.push_back(num_nhad);
-    jet_num_electron_.push_back(num_electron);
-    jet_num_muon_.push_back(num_muon);
-    jet_num_photon_.push_back(num_photon);
+    b_jet_num_chad_.push_back(num_chad);
+    b_jet_num_nhad_.push_back(num_nhad);
+    b_jet_num_electron_.push_back(num_electron);
+    b_jet_num_muon_.push_back(num_muon);
+    b_jet_num_photon_.push_back(num_photon);
 
     // Move CMSSW to higher version and use structured bindings
     Float_t major_axis, minor_axis, eccentricity;
     std::tie(major_axis, minor_axis, eccentricity) = ComputeAxes<Float_t>(
-        con_deta_.back(), con_dphi_.back(), con_pt_.back());
+        b_con_deta_.back(), b_con_dphi_.back(), b_con_pt_.back());
 
-    jet_major_axis_.push_back(major_axis);
-    jet_minor_axis_.push_back(minor_axis);
+    b_jet_major_axis_.push_back(major_axis);
+    b_jet_minor_axis_.push_back(minor_axis);
     // jet_eccentricity_.push_back(eccentricity);
 
     // jet energy sharing variable
     Float_t pt_l1_norm = std::accumulate(
-        con_pt_.back().begin(), con_pt_.back().end(), 0.0f);
+        b_con_pt_.back().begin(), b_con_pt_.back().end(), 0.0f);
     Float_t pt_l2_norm_squared = std::inner_product(
-        con_pt_.back().begin(), con_pt_.back().end(),
-        con_pt_.back().begin(), 0.0f); 
+        b_con_pt_.back().begin(), b_con_pt_.back().end(),
+        b_con_pt_.back().begin(), 0.0f); 
     Float_t ptd = std::sqrt(pt_l2_norm_squared) / pt_l1_norm;
-    jet_ptd_.push_back(ptd);
+    b_jet_ptd_.push_back(ptd);
 
     ////////////////////////////////////////////////////////////////////////////
     // NOTE b-jet properties
     ////////////////////////////////////////////////////////////////////////////
-    jet_b_tag_.push_back(jet->BTag);
-
+    b_jet_b_tag_.push_back(jet->BTag);
+    b_jet_b_tag_algo_.push_back(jet->BTagAlgo);
+    b_jet_b_tag_phys_.push_back(jet->BTagPhys);
 
     Bool_t found_matched_b = false;
-    for (const auto & b : bottom_quarks) {
-      Double_t delta_r = b->P4().DeltaR(jet->P4());
+    for (const auto & b_quark : bottom_quarks) {
+      Double_t delta_r = b_quark->P4().DeltaR(jet->P4());
       if (delta_r < kBMatchingDeltaR_) {
         found_matched_b = true;
         break;
       }
     }
-    jet_b_dr_matching_.push_back(found_matched_b);
+    b_jet_b_dr_matching_.push_back(found_matched_b);
 
     Float_t b_dau_ratio = getBDaughterRatio(jet);
-    jet_b_tracking_.push_back(b_dau_ratio);
+    b_jet_b_tracking_.push_back(b_dau_ratio);
 
   } // end loop over jets
 }
@@ -400,9 +451,9 @@ void TTAllJetsAnalyser::analyse() {
   fillJetVariables();
 
   auto missing_et = dynamic_cast<MissingET*>(mets_->At(0));
-  met_ = missing_et->MET;
-  met_eta_ = missing_et->Eta;
-  met_phi_ = missing_et->Phi;
+  b_met_ = missing_et->MET;
+  b_met_eta_ = missing_et->Eta;
+  b_met_phi_ = missing_et->Phi;
 
   out_tree_->Fill();
 }
@@ -426,5 +477,7 @@ void TTAllJetsAnalyser::loop() {
     if (selectEvent()) {
       analyse();
     }
+
+
   }
 }
