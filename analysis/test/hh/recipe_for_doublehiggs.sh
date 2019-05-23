5439a0e87ff2a9ab209b3705e7919f1fba59b171
Master Student from University of Seoul <syoo@gate.sscc.uos>
2019-05-23 12:20:33 +0900

    [Double Higgs] update the recipe for the analyser

--- analysis/test/hh/recipe_for_doublehiggs.sh
+++ analysis/test/hh/recipe_for_doublehiggs.sh
@@ -8,7 +8,6 @@ git clone git@github.com:"your_user_name"/delphys.git #"your_user_name" should b
 #build analysers 
 scram b -j 20
 getFiles
-
 #### Delphes  
 cd $CMSSW_BASE/src/delphys/external/interface/
 wget http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.4.1.tar.gz
@@ -18,43 +17,37 @@ cd Delphes-3.4.1
 sed -i s:c++0x:c++17: Makefile #set c++ version as 17th in Makefile
 make -j 4
 export LD_LIBRARY_PATH=$CMSSW_BASE/src/delphys/external/interface/Delphes-3.4.1:$LD_LIBRARY_PATH
+#make BuildFile.xml with the script
+./$CMSSW_BASE/src/delphys/analysis/test/makeBuildFile -d $CMSSW_BASE/src/delphys/external/interface/Delphes-3.4.1/
 #add delphes library path into the .bashrc (or .bash_profile)
 cat >> ~/.bashrc <<EOL
-#Delphes Library Path
+# Delphes Library Path
 export LD_LIBRARY_PATH=$CMSSW_BASE/src/delphys/external/interface/Delphes-3.4.1:\$LD_LIBRARY_PATH
 EOL
-
 #### Oxbridge
-cd $CMSSW_BASE/src/delphys/
+cd $CMSSW_BASE/src/delphys/external/
 wget http://www.hep.phy.cam.ac.uk/~lester/dtm662/mt2/Releases/oxbridgekinetics.tar.gz
 tar -xzvf oxbridgekinetics.tar.gz
 rm oxbridgekinetics.tar.gz
-mv oxbridgekinetics-1.2/ oxbridgekinetics
-cd oxbridgekinetics
-touch BuildFile.xml
+mv oxbridgekinetics-1.3/Mt2/ src/
+cd src/Mt2
+sed -i s:"Mt2/:": * # modify the path of Mt2 header files.
+cd ../../
+scram b
 #####################################################################
 #make a buildfile for integrating oxbridgekinetics library into nano
-#specify flags in BuildFile to ignore unnecessary warnings and errors
+#Todo : should be able to remove the error ignoring flags
 cat > BuildFile.xml << EOL
-<use name="root" />
 <flags LDFLAGS="-lMinuit2" />
 <flags CXXFLAGS="-Wno-error=unused-but-set-variable" />
 <flags CXXFLAGS="-Wno-error=unused-variable" />
 <flags CXXFLAGS="-Wno-error=sign-compare" />
 <flags CXXFLAGS="-Wno-error=maybe-uninitialized" />
-<export>
-  <lib name="1"/>
-</export>
 EOL 
 #####################################################################
-mv Mt2/ src
-cd src/Mt2
-sed -i s:"Mt2/:": * # modify the path of Mt2 header files.
-cd ../../
-scram b
 #set right path in the nano/analysis/bin/BuildFile.xml
-sed -i s:CMSSW_BASE:$CMSSW_BASE: $CMSSW_BASE/src/delphys/analysis/bin/BuildFile.xml
-sed -i '/erase/d' $CMSSW_BASE/src/delphys/analysis/bin/BuildFile.xml
+#Lines below should be added in the analysis/bin/BuildFile.xml
 cd $CMSSW_BASE/src/delphys/analysis/bin/
+sed -i 's,\(\doubleHiggsAnalyser.*\),\1\n  \<flags CXXFLAGS=\"-lMinuit\"/\>,g' $CMSSW_BASE/src/delphys/analysis/bin/BuildFile.xml
+
 scram b -j12
-doubleHiggsAnalyser
