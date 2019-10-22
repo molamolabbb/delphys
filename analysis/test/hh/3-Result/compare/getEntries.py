3bf2869ddef34a554d2cdc734c047abfe34d225d
juakim <gmldnjs0971@uos.ac.kr>
2019-10-22 09:23:14 +0900

    update

--- analysis/test/hh/3-Result/compare/getEntries.py
+++ analysis/test/hh/3-Result/compare/getEntries.py
@@ -10,45 +10,100 @@ cut_samples = ["pp_hh","pp_tt_all","pp_llbj","pp_tatabb","pp_tth","pp_ttv","pp_t
 
 chain = {"gen":"Delphes","cut":"events","bdt":"events"}
 cross_sig = 104.06
-cross_bg = 3901820.746
+cross_bg = {"pp_hh":104,"pp_tt_all":30655.20025,"pp_llbj":3450000,"pp_tatabb":7320,"pp_ttv":2459054.565,"pp_tth":1049562.954,"pp_twj":372465.0508}
 
-def getEntries(samples,path,cut):
+def getGenEntries(samples,path,cut):
   sig_entries = 0
-  bg_entries = 0
+  pp_tt_entries = 0
+  bg_entries = {}
   for sample in samples:
     tree = TChain(chain[cut])
     tree.Add(path+str(sample)+"/*.root")
-    if cut == "bdt":
-      entries = 0 
-      for e in tree:
-          if e.step==4 and e.tmva_bdtg_output>0.5:
-            entries=entries+1
-    else : entries = tree.GetEntries()
+    entries = tree.GetEntries()
+    bg_entries[sample] = entries
+    if (sample is "pp_tt") or (sample is "pp_tt_ditau") or(sample is "pp_tt_leptau"):
+      pp_tt_entries = pp_tt_entries+entries
     print("Number of {} : {}".format(sample, entries))
     if str(sample) == "pp_hh":
       sig_entries = entries
-    else :
-      bg_entries = bg_entries + entries  
+    bg_entries["pp_tt_all"] = pp_tt_entries
+  print(sig_entries,bg_entries)
+  print("=====================================")
+  return sig_entries, bg_entries
+
+def getEntries(samples,path,cut):
+  sig_entries = 0
+  pp_tt_entries = 0
+  bg_entries = {}
+  for sample in samples:
+    tree = TChain(chain[cut])
+    if cut == "bdt":
+      tree.Add(path+str(sample)+"2/*.root")
+      entries = 0 
+      for e in tree:
+        if e.step==4 and e.tmva_bdtg_output>0:
+          entries=entries+1
+    elif cut == "cut":
+      tree.Add(path+str(sample)+"2/*.root")
+      entries = 0 
+      for e in tree:
+          if e.step==4 :
+            entries=entries+1
+    if str(sample) == "pp_hh": 
+      sig_entries = entries
+      print("Number of {} : {}".format(sample, entries))
+    else : 
+      bg_entri = float(entries)*float(cross_bg[sample]/gen_bg[sample])
+      bg_entries[sample] = bg_entri
+      print("Number of {} : {}".format(sample, bg_entri))
+  print(sig_entries,bg_entries)
+  print("=====================================")
   return sig_entries, bg_entries
  
 
-gen_sig, gen_bg = getEntries(gen_samples,gen_path,"gen")
+gen_sig, gen_bg = getGenEntries(gen_samples,gen_path,"gen")
 cut_sig, cut_bg = getEntries(cut_samples,cut_path,"cut")
 bdt_sig, bdt_bg = getEntries(cut_samples,bdt_path,"bdt")
 
+num_bg = 0
+for sample in samples:
+  num_bg = num_bg+bdt_bg[sample]
+num_sig = bdt_sig*cross_sig/gen_sig 
+print(num_sig)
+print(num_bg)
+bdtSensitivity = num_sig/math.sqrt(num_bg+num_sig) 
+print("BDT Sensitivity : {}".format(bdtSensitivity))
+
+num_bg = 0
+for sample in samples:
+  num_bg = num_bg+cut_bg[sample]
+num_sig = cut_sig*cross_sig/gen_sig 
+print(num_sig)
+print(num_bg)
+cutSensitivity = num_sig/math.sqrt(num_bg+num_sig) 
+print("Cut Based Sensitivity : {}".format(cutSensitivity))
+
+'''
 print("Number of Gen backgrounds entries : {}".format(gen_bg))
 print("Number of Gen signal entries : {}".format(gen_sig))
 print("Number of cut backgrounds entries : {}".format(cut_bg))
 print("Number of cut signal entries : {}".format(cut_sig))
 print("Number of bdt backgrounds entries : {}".format(bdt_bg))
 print("Number of bdt signal entries : {}".format(bdt_sig))
-
+print("===================================================")
 num_sig = cut_sig*cross_sig/gen_sig 
 num_bg  = cut_bg*cross_bg/gen_bg
+print(num_sig)
+print(num_bg)
 genSensitivity = num_sig/math.sqrt(num_bg+num_sig) 
 print("Cut based Sensitivity : {}".format(genSensitivity))
+print("===================================================")
 
 num_sig = bdt_sig*cross_sig/gen_sig 
 num_bg  = bdt_bg*cross_bg/gen_bg
+
+print(num_sig)
+print(num_bg)
 bdtSensitivity = num_sig/math.sqrt(num_bg+num_sig) 
 print("BDT Sensitivity : {}".format(bdtSensitivity))
+'''
